005a0036b752b90fabcba73aeb69635256a01f21
Tyler Kleint <38107146+OffensiveGeneric@users.noreply.github.com>
2026-08-14 02:17:10 -0400

    Build out photography and blog experience

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 app_dir=/srv/tyler-site/app
 export SITE_DB_PATH=/srv/tyler-site/data/site.db
 
-install -d -m 0750 /srv/tyler-site/data /srv/tyler-site/media
+install -d -m 0750 /srv/tyler-site/data
+install -d -m 0755 /srv/tyler-site/media
+chmod 0755 /srv/tyler-site
 cd "$app_dir"
 git fetch origin master
 git reset --hard origin/master
